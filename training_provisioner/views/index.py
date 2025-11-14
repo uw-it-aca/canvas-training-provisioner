91cd4c67d37164186064ff56f67459037d02d656
mike seibel <mikes@washington.edu>
2025-11-13 17:05:09 -0800

    admin role redirect to admin site

--- training_provisioner/views/index.py
+++ training_provisioner/views/index.py
@@ -2,11 +2,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from training_provisioner.dao.group import is_admin_user
 
 
 @method_decorator(login_required, name='dispatch')
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if is_admin_user(request):
+            return redirect('admin:index')
+
+        return super().dispatch(request, *args, **kwargs)
