ac9f9a85ac3014d2e59c1bdab8bddb2f43beaae5
mike seibel <mikes@washington.edu>
2025-11-25 12:29:02 -0800

    sis import test

--- training_provisioner/test/dao/test_canvas.py
+++ training_provisioner/test/dao/test_canvas.py
@@ -32,3 +32,30 @@ class CanvasSISImportsTest(TestCase):
     def test_delete_sis_import(self, mock_method, mock_model):
         r = delete_sis_import('123')
         mock_model.assert_called_with(import_id='123')
+
+
+class CanvasSISImportsTest(TestCase):
+    @mock.patch('training_provisioner.dao.canvas.default_storage.listdir')
+    @mock.patch.object(SISImport, 'import_archive')
+    def test_sis_import_by_path(self, mock_method, mock_listdir):
+        mock_listdir.return_value = ((), ())
+
+        r = sis_import_by_path('abc')
+        mock_method.assert_called_with(ANY, params={})
+
+        r = sis_import_by_path('abc', override_sis_stickiness=True)
+        mock_method.assert_called_with(
+            ANY, params={
+                'override_sis_stickiness': '1', 'clear_sis_stickiness': '1'})
+
+    @mock.patch('training_provisioner.dao.canvas.SISImportModel')
+    @mock.patch.object(SISImport, 'get_import_status')
+    def test_get_sis_import_status(self, mock_method, mock_model):
+        r = get_sis_import_status('123')
+        mock_model.assert_called_with(import_id='123')
+
+    @mock.patch('training_provisioner.dao.canvas.SISImportModel')
+    @mock.patch.object(SISImport, 'delete_import')
+    def test_delete_sis_import(self, mock_method, mock_model):
+        r = delete_sis_import('123')
+        mock_model.assert_called_with(import_id='123')
