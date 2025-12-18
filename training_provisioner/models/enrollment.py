5011fde3b0a6f259f4ea39a51a749108daabbcce
mike seibel <mikes@washington.edu>
2025-12-18 14:31:21 -0800

    course change error message

--- training_provisioner/models/enrollment.py
+++ training_provisioner/models/enrollment.py
@@ -197,7 +197,9 @@ class EnrollmentManager(models.Manager):
                             f"{section_or_course_id}")
 
             if enrollment.course != course:
-                raise EnrollmentCourseMismatch()
+                raise EnrollmentCourseMismatch(
+                    f"Enrollment for {studentno} course change from "
+                    f"{enrollment.course != course} to {course} NOT allowed")
             elif enrollment.section != section:
                 orig_course_id = enrollment.course.course_id
                 orig_section_id = (enrollment.section.section_id
